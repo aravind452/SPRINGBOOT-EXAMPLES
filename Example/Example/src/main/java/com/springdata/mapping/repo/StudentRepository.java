47e66fda6ca62ed09e6e2076c2c46aefc37f1930
aravind452 <aravindaru452@gmail.com>
2024-11-30 18:03:13 +0530

    added custom queries in repo

--- Example/Example/src/main/java/com/springdata/mapping/repo/StudentRepository.java
+++ Example/Example/src/main/java/com/springdata/mapping/repo/StudentRepository.java
@@ -3,6 +3,7 @@ package com.springdata.mapping.repo;
 
 import com.springdata.mapping.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +20,11 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
 
     public List<Student> findByGuardianName(String guardianName);
+
+    // JPQL
+    @Query("select s from Student s where s.emailId = ?1")
+    public Student getStudentByEmailAddress(String emailId);
+
+    @Query("select s.firstName from Student s where s.emailId = ?1")
+    public String getStudentFirstNameByEmailAddress(String emailId);
 }
