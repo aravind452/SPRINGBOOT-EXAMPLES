f1f1daa8be8234db6fdd1f65d3977e076105a7cb
aravind452 <aravindaru452@gmail.com>
2024-11-30 21:26:54 +0530

    added one to one relationship

---
+++ Example/Example/src/main/java/com/springdata/mapping/repo/CourseMaterialRepository.java
@@ -0,0 +1,10 @@
+package com.springdata.mapping.repo;
+
+
+import com.springdata.mapping.model.CourseMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMaterialRepository extends JpaRepository<CourseMaterial, Long> {
+}
