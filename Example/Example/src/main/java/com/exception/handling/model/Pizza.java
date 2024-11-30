21dc80f46bca0c3bb56409951a71d6be79f18250
aravind452 <aravindaru452@gmail.com>
2024-11-30 13:32:04 +0530

    modified entity

--- Example/Example/src/main/java/com/exception/handling/model/Pizza.java
+++ Example/Example/src/main/java/com/exception/handling/model/Pizza.java
@@ -1,9 +1,9 @@
 package com.exception.handling.model;
 
-
 import jakarta.persistence.Entity;
-
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Pizza {
